d10d1c547e678ccc1e5d9f02c1deb068a6976601
aanyagarg <73021607+aanyagarg@users.noreply.github.com>
2022-01-06 08:57:45 +0530

    Add files via upload

---
+++ coviddata.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+import plotly.express as px
+
+
+df = pd.read_csv("Copy+of+data+-+data.csv")
+fig = px.bar(df, x='date', y='cases',color='country', title='Corona cases')
+fig.show()
